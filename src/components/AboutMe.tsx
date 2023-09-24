4c4aec91597359b9386165a45f5e0e6641513977
Suka <mateodefalco1@gmail.com>
2023-09-24 16:26:21 -0300

    AboutMe Updates

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -16,19 +16,37 @@ const AboutMe = () => {
 			<div className="flex flex-col gap-4 justify-center">
 				<h2 className="font-extrabold">ABOUT ME</h2>
 				<p className="text-slate-500">
-					Hello, I'm Mateo, a <b>full-stack web developer</b> with a lifelong
-					passion for technology. Over the past few years, I've dedicated myself
-					to refining my skills through a diverse range of{" "}
-					<b>freelance and individual projects</b>. Additionally, I have a solid
-					foundation in <b>blockchain and decentralized technologies</b>. I
-					strongly believe in the importance of continuous <b>learning</b>, and
-					I'm committed to staying updated with the latest advancements in the
-					field.
+					Hello, I'm Mateo, a{" "}
+					<b className="dark:text-white">full-stack web developer</b> with a
+					lifelong passion for technology. Over the past few years, I've
+					dedicated myself to refining my skills through a diverse range of{" "}
+					<b className="dark:text-white">freelance and individual projects</b>.
+					Additionally, I have a solid foundation in{" "}
+					<b className="dark:text-white">
+						blockchain and decentralized technologies
+					</b>
+					. I strongly believe in the importance of continuous{" "}
+					<b className="dark:text-white">learning</b>, and I'm committed to
+					staying updated with the latest advancements in the field.
 				</p>
 				<div className="flex gap-2 justify-center lg:justify-start">
-					<Button variant="outline" onClick={handleResumeClick}>
-						Resume
-					</Button>
+					<a
+						href="https://www.linkedin.com/in/mateo-de-falco/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-gray-500 hover:text-gray-700"
+					>
+						<Button variant="outline">LinkedIn</Button>
+					</a>
+					<a
+						href="https://github.com/matedefalco"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-gray-500 hover:text-gray-700"
+					>
+						<Button variant="outline">GitHub</Button>
+					</a>
+					<Button onClick={handleResumeClick}>Resume</Button>
 				</div>
 			</div>
 		</div>
