3e58d80bf28628e093e3f85095a31db45f326d48
Suka <mateodefalco1@gmail.com>
2023-09-29 18:45:18 -0300

    Project card fixes

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -7,28 +7,28 @@ const ProjectComponent: React.FC<ProjectProps> = ({ name, url, image }) => {
 	const isMobile = useMediaQuery({ maxWidth: 767 })
 
 	return (
-		<Card className="bg-white dark:bg-[--bg-secondary] min-h-full flex flex-col justify-start">
-			<CardContent>
-				<div className="flex flex-col items-center justify-center pt-4">
-					<img src={image} alt={image} className="object-cover w-full h-40" />
-				</div>
-			</CardContent>
-			<CardFooter>
-				<a
-					href={url}
-					target="_blank"
-					rel="noopener noreferrer"
-					className="text-xs text-slate-400 hover:underline"
-				>
+		<a
+			href={url}
+			target="_blank"
+			rel="noopener noreferrer"
+			className="text-xs text-slate-400 hover:underline"
+		>
+			<Card className="bg-white dark:bg-[--bg-secondary] min-h-full flex flex-col justify-start">
+				<CardContent>
+					<div className="flex flex-col items-center justify-center pt-4">
+						<img src={image} alt={image} className="object-cover w-full h-40" />
+					</div>
+				</CardContent>
+				<CardFooter className="flex flex-col items-center">
 					<div className="flex flex-col items-center justify-center lg:items-start lg:justify-start gap-2 w-full">
-						<div className="flex flex-col">
-							<h1 className="text-sm text-white font-bold">{name}</h1>
+						<div className="flex flex-col items-center">
+							<h1 className="text-sm font-bold">{name}</h1>
 							{!isMobile && <p className="text-xs text-slate-400">{url}</p>}
 						</div>
 					</div>
-				</a>
-			</CardFooter>
-		</Card>
+				</CardFooter>
+			</Card>
+		</a>
 	)
 }
 
