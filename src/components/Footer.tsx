b1094d2fb3185dc7e14456a9cc2ce27ce5ef3a6b
Suka <mateodefalco1@gmail.com>
2023-10-04 09:15:52 -0300

    NavBar upgrade

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,10 @@ import { IoLocationSharp } from "react-icons/io5"
 
 const Footer = () => {
 	return (
-		<div className="flex flex-col gap-8 items-center bg-slate-200 dark:bg-slate-900 p-8">
+		<div
+			id="footer"
+			className="flex flex-col gap-8 items-center bg-slate-200 dark:bg-slate-900 p-8"
+		>
 			<h1 className="font-extrabold text-2xl">CONTACT</h1>
 			<div className="flex flex-col lg:flex-row px-16 items-start justify-between gap-8 lg:gap-0">
 				<div className="flex flex-col w-full lg:w-[45%] gap-4 items-center lg:items-start text-center lg:text-start mx-auto lg:mx-0">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,13 @@ import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./ModeToggle"
 
 const NavBar = () => {
+	const handleScroll = () => {
+		const element = document.getElementById("footer")
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" })
+		}
+	}
+
 	return (
 		<div className="flex justify-between items-center w-full p-6 lg:p-4 border-b-2 sticky top-0 bg-white dark:bg-slate-900 z-50">
 			<div className="flex gap-2 items-center">
@@ -10,9 +17,9 @@ const NavBar = () => {
 			</div>
 			<div className="flex gap-2 items-center">
 				<ModeToggle />
-				<a href="mailto:mateodefalco1@gmail.com" target="_blank">
-					<Button variant="outline">CONTACT</Button>
-				</a>
+				<Button variant="outline" onClick={handleScroll}>
+					CONTACT
+				</Button>
 			</div>
 		</div>
 	)
