2f989f692e85adca50c826c35e9f534c9daef664
Suka <mateodefalco1@gmail.com>
2023-10-04 14:05:14 -0300

    Color mode upgrade

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,38 +1,18 @@
 import { Button } from "@/components/ui/button"
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { useTheme } from "@/context/ThemeProvider"
 
 export function ModeToggle() {
 	const { theme, setTheme } = useTheme()
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
-					<img
-						src={theme === "dark" ? "sun.png" : "moonIconLight.png"}
-						alt="Theme"
-						className="w-5 h-5"
-					/>
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>
-					Light
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>
-					Dark
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>
-					System
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<Button variant="outline" size="icon">
+			<img
+				src={theme === "dark" ? "sun.png" : "moonIconLight.png"}
+				alt="Theme"
+				className="w-5 h-5"
+				onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+			/>
+			<span className="sr-only">Toggle theme</span>
+		</Button>
 	)
 }
