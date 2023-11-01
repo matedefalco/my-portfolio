ea7f20ff2a29ecb04aaeb84b19bd054dff97341e
Suka <mateodefalco1@gmail.com>
2023-11-01 14:07:03 -0300

    Portfolio optimization

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,6 +5,12 @@ import Skills from "@/views/Skills"
 import Education from "@/views/Education"
 import { PortfolioProps } from "@/types/INavigation"
 
+const pages = [
+	{ name: "Projects", route: "projects" },
+	{ name: "Education", route: "education" },
+	{ name: "Stack", route: "skills" },
+]
+
 const Portfolio: React.FC<PortfolioProps> = ({ activePage }) => {
 	const [activeTab, setActiveTab] = useState("projects")
 
@@ -20,39 +26,20 @@ const Portfolio: React.FC<PortfolioProps> = ({ activePage }) => {
 		<div id="portfolio" className="flex flex-col items-center gap-4">
 			<h1 className="font-extrabold text-3xl font-serif">PORTFOLIO</h1>
 			<div className="flex justify-between items-center mt-4 border rounded-lg py-2 px-4 bg-[--primary-color]">
-				<Button
-					variant="link"
-					onClick={() => handleTabChange("skills")}
-					className={
-						activeTab === "skills"
-							? "text-lg text-white dark:text-white font-bold"
-							: "text-md text-gray-300 dark:text-gray-300"
-					}
-				>
-					Stack
-				</Button>
-				<Button
-					variant="link"
-					onClick={() => handleTabChange("projects")}
-					className={
-						activeTab === "projects"
-							? "text-lg text-white dark:text-white font-bold"
-							: "text-md text-gray-300 dark:text-gray-300"
-					}
-				>
-					Projects
-				</Button>
-				<Button
-					variant="link"
-					onClick={() => handleTabChange("education")}
-					className={
-						activeTab === "education"
-							? "text-lg text-white dark:text-white font-bold"
-							: "text-md text-gray-300 dark:text-gray-300"
-					}
-				>
-					Education
-				</Button>
+				{pages.map((page, index) => (
+					<Button
+						key={index}
+						variant="link"
+						onClick={() => handleTabChange(pages[index].route)}
+						className={
+							activeTab === pages[index].route
+								? "text-lg text-white dark:text-white font-bold"
+								: "text-md text-gray-300 dark:text-gray-300"
+						}
+					>
+						{page.name}
+					</Button>
+				))}
 			</div>
 			<div className="flex flex-col z-0">
 				{activeTab === "projects" ? <Projects /> : null}
